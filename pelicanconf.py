9c9e421a2eea59f1408b852c55d6d67419153f94
risobo <michael.diks@gmail.com>
2025-01-26 20:25:06 +0100

    Add Pelican plugins

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,15 @@
-AUTHOR = 'Mindwrapper'
-SITENAME = 'Mindwrapper'
+AUTHOR = "Mindwrapper"
+SITENAME = "Mindwrapper"
 SITEURL = ""
 
 PATH = "content"
 
-TIMEZONE = 'Europe/Amsterdam'
+TIMEZONE = "Europe/Amsterdam"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
+
+THEME = "themes/Flex"
+PLUGINS = ["sitemap", "neighbors"]  # Add plugins if needed
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -22,6 +25,12 @@ LINKS = (
     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
     ("You can modify those links in your config file", "#"),
 )
+# sitemap
+
+SITEMAP = {
+    "format": "xml",
+    "priorities": {"articles": 0.5, "indexes": 0.5, "pages": 0.5},
+}
 
 # Social widget
 SOCIAL = (
