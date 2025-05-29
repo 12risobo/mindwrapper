bc5df9112af8a26bb98cbf67bcf187be5e1cd99e
risobo <michael.diks@gmail.com>
2025-05-29 10:35:20 +0200

    Fix homepage config

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -82,7 +82,7 @@ const config: QuartzConfig = {
       Plugin.TagPage(),
       Plugin.ContentIndex({
         enableSiteMap: true,
-        enableRSS: true,
+        enableRSS: false,
       }),
       Plugin.Assets(),
       Plugin.Static(),
